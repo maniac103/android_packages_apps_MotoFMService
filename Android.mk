9333c4b7f89d848bd285c3089090a966ed5582ac
Danny Baumann <dannybaumann@web.de>
2012-07-19 18:50:02 +0200

    Fix launching service
    - Need to use the platform certificate due to system UID.
    - Need to disable proguard, otherwise the JNI doesn't find the callback
      method.

--- Android.mk
+++ Android.mk
@@ -7,5 +7,7 @@ LOCAL_SRC_FILES := $(call all-subdir-java-files) \
 	src/com/motorola/android/fmradio/IFMRadioService.aidl \
 
 LOCAL_PACKAGE_NAME := MotoFMService
+LOCAL_CERTIFICATE := platform
+LOCAL_PROGUARD_ENABLED := disabled
 
 include $(BUILD_PACKAGE)
